be54d37f606f17113807ee1fc44b73605fbf347c
Naveen Subramani <Naveen Subramani>
2014-08-27 15:13:01 +0530

    Basic Template

--- selsite.py
+++ selsite.py
@@ -1,1 +1,16 @@
 __author__ = 'naveen'
+
+
+import os
+from flask import Flask, redirect, render_template, send_file, url_for, request, flash, session, abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return "Welcome to pin labs :)"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(32)
+    app.run('0.0.0.0', 5000,debug=True)
